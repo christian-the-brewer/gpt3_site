140b039b944f30f1b8cbcb6fd2ae4c1260a397bc
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-25 08:21:16 -0500

    downloads assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import {Footer, Blog, Possibility, Feature, WhatGPT3, Header, Features} from "./containers";
-import { CTA, Brand, Navbar } from './components';
+import React, {useState} from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  WhatGPT3,
+  Header,
+  Features,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css"
 
 const app = () => {
   return (
-    <div>
-        <div className='App'>
-          <div className='gradient__bg'>
-            <Navbar />
-            <Header />
-          </div>
-          <Brand />
-          <WhatGPT3 />
-          <Features />
-          <Possibility />
-          <CTA />
-          <Blog />
-          <Footer />
-        </div>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>      
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default app
+export default app;
